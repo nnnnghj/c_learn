42f9997e79a87a6abee2dd30168f0fce15e72839
nnnnghj <1290618843@qq.com>
2022-10-09 16:47:49 +0800

    第二次打卡，数据类型，变量类型

---
+++ test_9_26/test_9_26/test.c
@@ -0,0 +1,112 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+//int main()
+//{
+//	printf("haha\n");
+//
+//	return 0;
+//}
+
+
+//数据类型
+//计算机语言 - 写程序 - 解决生活中问题
+//必须有能力描述生活中问题
+//购物商场 - 上架商品，价格 - 15.6元 - 小数
+//浮点数
+
+//年龄 - 50
+//     - 整型
+
+
+//a
+//'a' - 字符
+
+//int main() {
+//
+//	//字符类型
+//	char ch = 'a';
+//
+//	//整型
+//	int age = 20;
+//	
+//	//短整型
+//	short num = 10;
+//	//long 
+//	//long long
+//
+//	//单精度浮点型
+//	float weight = 55.5;
+//
+//	//双精度浮点型
+//	double d = 0.0;
+//
+//
+//	return 0;
+//}
+
+////全局变量
+//int a = 100;//大括号外定义的为全局变量
+
+//int main()
+//{
+//	//printf("hehe\n");
+//	//printf("比特\n");
+//
+//	//printf("%d\n",100);//打印一个整数
+//
+//	//sizeof - 关键字 - 操作符 - 计算类型或者变量所占空间大小
+//
+//
+//	/*printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));*/
+//	
+//	//单位是字节
+//	//bit - 比特位 - 存放一个二进制单位
+//	//byte - 字节 - 8bit
+//	//kb - 1024byte
+//	//mb - 1024kb
+//	//gb - 1024mb
+//	//tb - 1024gb
+//	//pb - 1024tb
+//
+//    //变量
+//	//类型 变量的名字 = 0//推荐
+//	//类型 变量的名字；//不推荐
+//
+//	//int age = 25;
+//	//double weight = 90.4;
+//
+//	//age = age + 1;
+//	//weight = weight - 10;
+//	//printf("%d\n", age);
+//	//printf("%lf\n", weight);
+//
+//	////%d - 整型
+//	////%f - float
+//	////%lf - double
+//
+//	////变量：部分变量、全局变量
+//	//int a = 10;//大括号内的都称为局部变量
+//	//printf("%d", a);//局部变量和全局变量冲突的情况下，局部优先
+//	////不建议把全局变量和局部变量同名
+//
+//	//求两个整数的和
+//
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	scanf("%d %d", &a, &b);//scanf函数是输入函数
+//	sum = a + b;
+//	printf("%d", sum);
+//
+//	return 0;
+//}
+
+
