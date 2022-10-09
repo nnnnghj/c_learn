11fed1591b937a0d222efc4d0d2ad202545f802c
nnnnghj <1290618843@qq.com>
2022-10-09 16:53:53 +0800

    第五次打卡，初识选择语句和循环语句，初识函数，初识操作符

---
+++ test_9_29/test_9_29/test.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+
+//int main()
+//{
+//	//选择语句
+//	/*int a = 0;
+//	printf("你是否好哈学习<1/0>：");
+//	scanf("%d", &a);
+//
+//	if (a = 1)
+//	{
+//		printf("nb");
+//	}
+//	else
+//	{
+//		printf("no");
+//	}*/
+//
+//	//循环语句
+//	/*int line = 0;
+//
+//	while (line<30000)
+//	{
+//		printf("写代码:%d\n", line);
+//		line++;
+//	}
+//
+//	if (line == 30000)
+//	{
+//		printf("nb\n");
+//	}*/
+//
+//	return 0;
+//}
+
---
+++ test_9_29/test_9_29/test1.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//函数
+//我：张三 带饭（蛋炒饭， 20）
+//张三（蛋炒饭， 20）
+
+//add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	//int num1 = 0;
+//	//int num2 = 0;
+//
+//	//scanf("%d%d", &num1, &num2);
+//
+//	///*int sum = 0;
+//	//sum = num1 + num2;*/
+//
+//	////函数的方式
+//	//int sum = add(num1, num2);
+//
+//	//printf("%d\n",sum);
+//
+//	/*int a = 1;
+//	int b = 2;
+//	int c = 3;*/
+//
+//	//...
+//
+//	//数组 - 一组相同类型的元素的集合
+//	//10个整型1~10存起来
+//	//int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	//char ch[6] = { 'a','b','c' };//不完全初始化，剩余的默认为0
+//	////数组是用下表来访问的
+//	//int i = 0;
+//	//while (i<10)
+//	//{
+//	//	printf("%d\n", arr[i]);
+//	//	i++;
+//	//}
+//
+//	return 0;
+//}
