ea5bc7f5525b7e903dd7c1aaa4124a881c951b34
nnnnghj <1290618843@qq.com>
2022-10-09 17:06:36 +0800

    第八次打卡，指针剩余的知识，以及结构体的有关知识

---
+++ test_10_09/test_10_09/test0.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//昨天指针还剩一点
+
+//指针 就是 地址
+
+//int main()
+//{
+//	int a = 10;
+//
+//	int* pa = &a;
+//
+//	*pa = 20;//* 解引用操作符 *pa就是通过pa里边的地址，找到a
+//
+//	printf("%d\n", a);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%d\n", sizeof(char*));
+//		printf("%d\n", sizeof(short*));
+//		printf("%d\n", sizeof(int*));
+//		printf("%d\n", sizeof(long*));
+//		printf("%d\n", sizeof(long long*));
+//		printf("%d\n", sizeof(float*));
+//		printf("%d\n", sizeof(double*));
+//
+//		//指针的大小是相同的
+//		//为什么呢？
+//		//指针是用来存放地址的
+//		//指针需要多大空间，取决与地址存储需要多大空间
+//
+//		//32位 32bit - 4byte
+//		//64位 64bit - 8byte
+//
+//	return 0;
+//}
