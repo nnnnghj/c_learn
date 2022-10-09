6b5171980721f933adbd9d737f0c78c8a44f0741
nnnnghj <1290618843@qq.com>
2022-10-09 16:43:05 +0800

    第一次打卡，重拾c语言

---
+++ test_12_5/test_12_5/test.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	//printf("hello\n");
+	int a = 0;
+	scanf("%d", &a);
+	printf("n = %d",a);
+	return 0;
+}
+
+//newc++file.cpp
